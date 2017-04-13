dab8a2045f6838440ecb0f3958ac336595d932ee
Mateusz Potasnik <Mateusz@MacBook-Pro-Mat.local>
2017-04-13 20:44:56 +0200

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Mateusz Potasnik on 13.04.2017.
+//  Copyright © 2017 Mateusz Potasnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
